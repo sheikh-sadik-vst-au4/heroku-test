0881a16c8c08d9ff58a70e42e4ed22ac7dfbfafa
sheikh-sadik-vst-au4 <mohdsadik.sadik@gmail.com>
2020-04-24 17:02:45 +0530

    done

--- frontend/build/static/js/main.746531e1.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[0],[,,,function(e,n,t){e.exports=t(10)},,,,,function(e,n,t){},function(e,n,t){},function(e,n,t){"use strict";t.r(n);var a=t(0),r=t.n(a),c=t(2),o=t.n(c);t(8),t(9);var l=function(){return r.a.createElement("div",{className:"App"},r.a.createElement("h1",null,"hello welcome react apps"))};o.a.render(r.a.createElement(r.a.StrictMode,null,r.a.createElement(l,null)),document.getElementById("root"))}],[[3,1,2]]]);
-//# sourceMappingURL=main.746531e1.chunk.js.map
