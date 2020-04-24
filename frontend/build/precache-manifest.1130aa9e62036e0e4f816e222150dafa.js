e4b109d581c2f131f7ed8fce7111ec5311223127
sheikh-sadik-vst-au4 <mohdsadik.sadik@gmail.com>
2020-04-24 15:36:56 +0530

    done

---
+++ frontend/build/precache-manifest.1130aa9e62036e0e4f816e222150dafa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89b7a02d5f4bf9f5a838e5c51ebf494b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42eea0058642fe4eb707",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "180e3b128c22821acd61",
+    "url": "/static/js/2.22f2546a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.22f2546a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42eea0058642fe4eb707",
+    "url": "/static/js/main.746531e1.chunk.js"
+  },
+  {
+    "revision": "700d31d1c6f37b7bdc51",
+    "url": "/static/js/runtime-main.84578d5e.js"
+  }
+]);
